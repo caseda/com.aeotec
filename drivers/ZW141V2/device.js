776ca214fdb7aab7f4cfccbba4a2ed2d215e8873
Aeotec-ccheng <63321041+Aeotec-ccheng@users.noreply.github.com>
2020-08-21 04:54:49 -0700

    feat: Nano Shutter Firmware V2.0 (ZW141V2) integration (#209)

---
+++ drivers/ZW141V2/device.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+
+class ZW141V2 extends ZwaveDevice {
+	
+	onMeshInit() {
+        this.registerCapability('onoff', 'SWITCH_BINARY');
+        this.registerCapability('windowcoverings_set', 'SWITCH_MULTILEVEL');
+
+        this.registerReportListener('BASIC', 'BASIC_REPORT', (report) => {
+		  this.setCapabilityValue('onoff', !!report.Value);
+        });
+		
+		this.registerReportListener('SWITCH_MULTILEVEL', 'SWITCH_MULTILEVEL_REPORT', (report) => {
+          this.setCapabilityValue('windowcoverings_set', !!report.Value);
+        });
+    }
+
+}
+
+module.exports = ZW141V2;
