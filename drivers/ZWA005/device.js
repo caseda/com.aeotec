ff08f535004102250cf59e0fd8f225a411374240
Aeotec-ccheng <63321041+Aeotec-ccheng@users.noreply.github.com>
2020-08-21 04:51:03 -0700

    feat: TriSensor (ZWA005) integration (#200)
    * ZWA005 integration
    
    TriSensor Integration
    
    * Updated Parameter setting details
    
    Lines changed:
    10
    29
    151
    192
    245
    264
    279
    298
    317
    336

---
+++ drivers/ZWA005/device.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+
+class AeotecTriSensorDevice extends ZwaveDevice {
+
+	onMeshInit() {
+		this._cancellationTimeout = this.getSetting('tamper_cancellation');
+
+		this.registerCapability('measure_battery', 'BATTERY');
+
+		this.registerCapability('alarm_motion', 'SENSOR_BINARY');
+		
+		
+
+		this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
+		this.registerCapability('measure_luminance', 'SENSOR_MULTILEVEL');
+
+
+		this.registerSetting('201', (value) => {
+			return new Buffer([Math.round(value*10), 1]);
+		});
+	}
+
+	onSettings(oldSettings, newSettings, changedKeys) {
+        if (changedKeys.includes('tamper_cancellation')) {
+			this._cancellationTimeout =  this.getSetting('tamper_cancellation');
+		}
+
+		return super.onSettings(oldSettings, newSettings, changedKeys);
+    }
+}
+
+module.exports = AeotecTriSensorDevice;
