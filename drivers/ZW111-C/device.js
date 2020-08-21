fd58731c86382f4f8b1f9c8c0442f07d15594663
Robin Bolscher <RobinBol@users.noreply.github.com>
2020-08-21 14:02:02 +0200

    feat: update Nano Dimmer (ZW111) parameter settings (#220)
    * Update to Nano Dimmer (ZW111) parameter settings.
    
    - Change Parameter 101 to value 8
    - Change Parmaeter 111 to value 1800
    - Add configuration parameter 101
    - Add configuration parameter 102
    - Add configuration parameter 103
    - Add configuration parameter 111
    - Add configuration parameter 112
    - Add configuration parameter 113
    - Add configuration parameter 249
    - Add configuration parameter 255
    
    * Re-update Nano Dimmer ZW111 driver
    
    -Wrong version used. Overwrite.
    -Changelog is the same.
    
    * fix(ZW111C): check for newly added capabilities
    
    Co-authored-by: Aeotec-ccheng <63321041+Aeotec-ccheng@users.noreply.github.com>

--- drivers/ZW111-C/device.js
+++ drivers/ZW111-C/device.js
@@ -1,20 +1,22 @@
 'use strict';
 
-const { ZwaveDevice } = require('homey-meshdriver');
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class ZW111C extends ZwaveDevice {
 
-  onMeshInit() {
-    this.registerCapability('onoff', 'SWITCH_BINARY');
-    this.registerCapability('dim', 'SWITCH_MULTILEVEL');
+	onMeshInit() {
+        this.registerCapability('onoff', 'SWITCH_BINARY');
+        this.registerCapability('dim', 'SWITCH_MULTILEVEL');
 
-    this.registerCapability('measure_power', 'METER');
-    this.registerCapability('meter_power', 'METER');
+        this.registerCapability('measure_power', 'METER');
+        this.registerCapability('meter_power', 'METER');
+		    if (this.hasCapability('measure_voltage')) this.registerCapability('measure_voltage', 'METER');
+        if (this.hasCapability('measure_current')) this.registerCapability('measure_current', 'METER');
 
-    this.registerReportListener('BASIC', 'BASIC_SET', report => {
-      this.setCapabilityValue('onoff', !!report.Value);
-    });
-  }
+        this.registerReportListener('BASIC', 'BASIC_SET', (report) => {
+          this.setCapabilityValue('onoff', !!report.Value);
+        });
+    }
 
 }
 
