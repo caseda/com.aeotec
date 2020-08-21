185bc88d2ab101be16ea3265c1c6ab07e295597e
Aeotec-ccheng <63321041+Aeotec-ccheng@users.noreply.github.com>
2020-08-21 05:48:40 -0700

    Update to Nano Switch (ZW116) Parameter Setttings. (#204)
    - Set Type from light to socket
    - Change Parameter 101 to value 1
    - Change Parmaeter 111 to value 1800
    - Add configuration parameter 101
    - Add configuration parameter 102
    - Add configuration parameter 103
    - Add configuration parameter 111
    - Add configuration parameter 112
    - Add configuration parameter 113

--- drivers/ZW116/device.js
+++ drivers/ZW116/device.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const { ZwaveDevice } = require('homey-meshdriver');
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class ZW116 extends ZwaveDevice {
-
-  onMeshInit() {
-    this.registerCapability('onoff', 'SWITCH_BINARY');
-    this.registerCapability('measure_power', 'METER');
-    this.registerCapability('meter_power', 'METER');
-
-    this.registerReportListener('BASIC', 'BASIC_SET', report => {
-      this.setCapabilityValue('onoff', !!report.Value);
-    });
-  }
-
+	
+	onMeshInit() {
+		this.registerCapability('onoff', 'SWITCH_BINARY');
+		this.registerCapability('measure_power', 'METER');
+		this.registerCapability('meter_power', 'METER');
+		
+		this.registerReportListener('BASIC', 'BASIC_SET', (report) => {
+			this.setCapabilityValue('onoff', !!report.Value);
+		});
+	}
+	
 }
 
 module.exports = ZW116;
