6d61dae021028cbfdfa5d5cf46206177aeffecee
Caseda <caseda08@hotmail.com>
2020-09-06 14:31:10 +0200

    fix nanomote quad

--- drivers/ZWA003/device.js
+++ drivers/ZWA003/device.js
@@ -2,13 +2,10 @@
 
 const { ZwaveDevice } = require('homey-meshdriver');
 
-class ZW130 extends ZwaveDevice {
+class ZWA003 extends ZwaveDevice {
 
   onMeshInit() {
-    // TODO: add battery type (INTERNAL) to driver.compose
-    this._batteryTrigger = this.getDriver().batteryTrigger;
     this._sceneTrigger = this.getDriver().sceneTrigger;
-    this._dimTrigger = this.getDriver().dimTrigger;
 
     this.registerCapability('measure_battery', 'BATTERY');
 
@@ -23,66 +20,6 @@ class ZW130 extends ZwaveDevice {
         this._sceneTrigger.trigger(this, null, data);
       }
     });
-    this.registerReportListener('CONFIGURATION', 'CONFIGURATION_REPORT', report => {
-      if (report['Parameter Number'] !== undefined
-        && report['Configuration Value'] !== undefined) {
-        if (report['Parameter Number'] === 9) {
-          const data = {
-            button: report['Configuration Value'][0].toString(),
-            scene: (report['Configuration Value'][1] === 1) ? 'Key Slide Up' : 'Key Slide Down',
-          };
-          this._sceneTrigger.trigger(this, null, data);
-        }
-        if (report['Parameter Number'] === 10) {
-          let value = Math.round(report['Configuration Value'][2] / 2) / 100;
-          if (value < 0.5) value = Math.max(value - 0.05, 0);
-          const token = {
-            dim: value,
-          };
-          const data = {
-            button: report['Configuration Value'][0].toString(),
-          };
-          this._dimTrigger.trigger(this, token, data);
-        }
-      }
-    });
-  }
-
-  // eslint-disable-next-line consistent-return
-  async onSettings(oldSettings, newSettings, changedKeys) {
-    await super.onSettings(oldSettings, newSettings, changedKeys);
-
-    if (changedKeys.includes('rgb_name')
-      || changedKeys.includes('rgb_r')
-      || changedKeys.includes('rgb_g')
-      || changedKeys.includes('rgb_b')) {
-      this.log('color changed');
-
-      if (newSettings.rgb_name === 'custom'
-        && newSettings['rgb_r'] !== undefined
-        && newSettings['rgb_g'] !== undefined
-        && newSettings['rgb_b'] !== undefined) {
-        this.log('custom color');
-        return this.configurationSet({
-          index: 5,
-          size: 4,
-        }, Buffer.from([newSettings.rgb_r, newSettings.rgb_g, newSettings.rgb_b, 0]));
-      }
-      this.log('listed color');
-
-      const valueArray = newSettings.rgb_name.split(',');
-      const multiplier = newSettings.rgb_name_level / 100 || 1;
-
-      return this.configurationSet({
-        index: 5,
-        size: 4,
-      }, Buffer.from([
-        Math.round(valueArray[0] * multiplier),
-        Math.round(valueArray[1] * multiplier),
-        Math.round(valueArray[2] * multiplier), 0]));
-    }
-
-    this.log(changedKeys);
   }
 
   sceneRunListener(args, state) {
@@ -98,16 +35,6 @@ class ZW130 extends ZwaveDevice {
     return Promise.reject(new Error('Button or scene undefined in args or state'));
   }
 
-  dimRunListener(args, state) {
-    if (!args) return Promise.reject(new Error('No arguments provided'));
-    if (!state) return Promise.reject(new Error('No state provided'));
-
-    if (args.button !== undefined && state.button !== undefined) {
-      return (args.button === state.button);
-    }
-    return Promise.reject(new Error('Button undefined in args or state'));
-  }
-
 }
 
-module.exports = ZW130;
+module.exports = ZWA003;
--- drivers/ZWA003/driver.js
+++ drivers/ZWA003/driver.js
@@ -2,20 +2,17 @@
 
 const Homey = require('homey');
 
-class ZW130Driver extends Homey.Driver {
+class ZWA003Driver extends Homey.Driver {
 
   onInit() {
     super.onInit();
 
-    this.batteryTrigger = new Homey.FlowCardTriggerDevice('zw130_battery_full').register();
-    this.sceneTrigger = new Homey.FlowCardTriggerDevice('zw130_scene').register().registerRunListener((args, state) => {
+    this.batteryTrigger = new Homey.FlowCardTriggerDevice('zwa003_battery_full').register();
+    this.sceneTrigger = new Homey.FlowCardTriggerDevice('zwa003_scene').register().registerRunListener((args, state) => {
       return args.device.sceneRunListener(args, state);
-    });
-    this.dimTrigger = new Homey.FlowCardTriggerDevice('zw130_dim').register().registerRunListener((args, state) => {
-      return args.device.dimRunListener(args, state);
     });
   }
 
 }
 
-module.exports = ZW130Driver;
+module.exports = ZWA003Driver;
